4f92cd0d520cb6289d07b0908b254fc6adb8106d
malmaslamani <ma4781@ship.edu>
2016-08-31 09:18:53 -0400

    initializing files for assignment 2 part A

---
+++ desktop/src/com/almaslamanigdx/game/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.almaslamanigdx.game.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.almaslamanigdx.game.AlmaslamaniGdxGame;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new AlmaslamaniGdxGame(), config);
+	}
+}
