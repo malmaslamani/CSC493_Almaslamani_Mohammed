f96aa249b34f5d0efaa23fe326ac1f43ffa1c281
malmaslamani <malmaslamani@hotmail.com>
2016-09-09 12:47:07 -0400

    Revert "ended chapter 3 code"
    This reverts commit 112a1c142c39cc7d0ddf695965d068387f5643c0.
    
    # Conflicts:
    #	core/src/com/almaslamanigdx/game/WorldController.java

--- desktop/src/com/almaslamanigdx/game/desktop/DesktopLauncher.java
+++ desktop/src/com/almaslamanigdx/game/desktop/DesktopLauncher.java
@@ -1,13 +1,12 @@
 package com.almaslamanigdx.game.desktop;
 
-import com.almaslamanigdx.game.CanyonBunnyMain;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
-//import com.almaslamanigdx.game.AlmaslamaniGdxGame;
+import com.almaslamanigdx.game.AlmaslamaniGdxGame;
 
 public class DesktopLauncher {
 	public static void main (String[] arg) {
 		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
-		new LwjglApplication(new CanyonBunnyMain(), config);
+		new LwjglApplication(new AlmaslamaniGdxGame(), config);
 	}
 }
